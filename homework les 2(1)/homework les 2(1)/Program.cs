bc03ac0563bc4c869ed3faada2a0aa682f530b1d
S1gnes <a.onis1308@gmail.com>
2023-06-11 20:12:35 +0300

    Counter Strike: Global Offensive

---
+++ homework les 2(1)/homework les 2(1)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace homework_les_2_1_
+{
+    class Program
+    {
+        static string SplitNumber(int number)
+        {
+            string result = "";
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                result = digit + " " + result;
+                number /= 10;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            bool banana = true;
+            while (banana)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number >= 10000 && number <= 99999)
+                {
+                    Console.WriteLine(SplitNumber(number));
+                }
+                else
+                {
+                    Console.WriteLine("Number is NOT five digit");
+                }
+            }
+        }    
+    }
+}
---
+++ homework les 3/homework les 3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static bool CheckIfPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            int divisor = 2;
+
+            while (number > divisor)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+
+                divisor++;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                bool isPrime = CheckIfPrime(number);
+                if (isPrime)
+                {
+                    Console.WriteLine(number + " is a prime number");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is NOT a prime number");
+                }
+            }
+        }
+    }
+}
