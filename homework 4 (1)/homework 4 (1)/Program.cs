b3b95ce0b505e005c10f9edfadeb5fa7cb6a29bb
S1gnes <135260559+S1gnes@users.noreply.github.com>
2023-06-18 22:39:46 +0300

    4 homwork

---
+++ homework 4 (1)/homework 4 (1)/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("5 array numbers:");
+int[] numbers = new int[5];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Your number:");
+int searchNumber = int.Parse(Console.ReadLine());
+int position = Array.IndexOf(numbers, searchNumber) + 1;
+int x = 0;
+
+foreach (int item in numbers)
+{
+    if (item == searchNumber)
+        Console.WriteLine($"Number found at position {position}");
+    else
+    {
+        x++;
+        if (x == 5)
+            Console.WriteLine("The number {0} was not found in the array.", searchNumber);
+    }
+}
---
+++ homework 4 (2)/homework 4 (2)/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = {1,2,3,4,5};
+int length = numbers.Length;
+int[] reversedNumbers = new int[length];
+for (int i = 0; i < length; i++)
+{
+    reversedNumbers[i] = numbers[length - i - 1];
+}
+foreach (int number in reversedNumbers)
+{
+    Console.WriteLine(number);
+}
